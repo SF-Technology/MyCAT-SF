4880dcc8022cef46420b537f5c6a0e6ba60e9e33
顺丰科技开源 <opensource@sfmail.sf-express.com>
2016-10-16 22:46:37 +0800

    优化RowDataCmp的比较
    原先修复order by 排序数值类型出错的bug需要根据数值的字符串字节得到数值字节再进行比较，这里优化为直接使用数值的字符串字节进行比较

--- src/main/java/org/opencloudb/mpp/tmp/RowDataCmp.java
+++ src/main/java/org/opencloudb/mpp/tmp/RowDataCmp.java
@@ -1,10 +1,8 @@
 package org.opencloudb.mpp.tmp;
 
-import org.opencloudb.mpp.ColMeta;
 import org.opencloudb.mpp.OrderCol;
 import org.opencloudb.mpp.RowDataPacketSorter;
 import org.opencloudb.net.mysql.RowDataPacket;
-import org.opencloudb.util.ByteUtil;
 
 import java.util.Comparator;
 
@@ -31,37 +29,6 @@ public class RowDataCmp implements Comparator<RowDataPacket> {
 			int colIndex = tmp[i].colMeta.colIndex;
 			byte[] left = o1.fieldValues.get(colIndex);
 			byte[] right = o2.fieldValues.get(colIndex);
-			
-			int colType = tmp[i].colMeta.colType;
-			switch (colType) {
-				case ColMeta.COL_TYPE_INT:
-				case ColMeta.COL_TYPE_INT24:
-					left = ByteUtil.getBytes(ByteUtil.getInt(left));
-					right = ByteUtil.getBytes(ByteUtil.getInt(right));
-					break;
-				case ColMeta.COL_TYPE_SHORT:
-					left = ByteUtil.getBytes(ByteUtil.getShort(left));
-					right = ByteUtil.getBytes(ByteUtil.getShort(right));
-					break;
-				case ColMeta.COL_TYPE_LONG:
-				case ColMeta.COL_TYPE_LONGLONG:
-					left = ByteUtil.getBytes(ByteUtil.getLong(left));
-					right = ByteUtil.getBytes(ByteUtil.getLong(right));
-					break;
-				case ColMeta.COL_TYPE_FLOAT:
-					left = ByteUtil.getBytes(ByteUtil.getLong(left));
-					right = ByteUtil.getBytes(ByteUtil.getLong(right));
-					break;
-				case ColMeta.COL_TYPE_DOUBLE:
-				case ColMeta.COL_TYPE_DECIMAL:
-				case ColMeta.COL_TYPE_NEWDECIMAL:
-					left = ByteUtil.getBytes(ByteUtil.getDouble(left));
-					right = ByteUtil.getBytes(ByteUtil.getDouble(right));
-					break;
-				default:
-					break;
-			}
-			
 			if (tmp[i].orderType == type) {
 				cmp = RowDataPacketSorter.compareObject(left, right, tmp[i]);
 			} else {

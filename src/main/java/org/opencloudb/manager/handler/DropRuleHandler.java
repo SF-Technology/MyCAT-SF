85ab6ff1ee4f8d15316f0f6c2f10ab369e505ee1
顺丰科技开源 <opensource@sfmail.sf-express.com>
2017-03-13 14:06:48 +0800

    当rule被table引用时，不可被删除

--- src/main/java/org/opencloudb/manager/handler/DropRuleHandler.java
+++ src/main/java/org/opencloudb/manager/handler/DropRuleHandler.java
@@ -9,6 +9,9 @@ import org.opencloudb.MycatConfig;
 import org.opencloudb.MycatServer;
 import org.opencloudb.config.ErrorCode;
 import org.opencloudb.config.loader.xml.jaxb.RuleJAXB;
+import org.opencloudb.config.model.SchemaConfig;
+import org.opencloudb.config.model.TableConfig;
+import org.opencloudb.config.model.rule.RuleConfig;
 import org.opencloudb.config.model.rule.TableRuleConfig;
 import org.opencloudb.config.util.JAXBUtil;
 import org.opencloudb.manager.ManagerConnection;
@@ -18,44 +21,63 @@ import org.opencloudb.route.function.AbstractPartitionAlgorithm;
 
 /**
  * 处理drop rule语句的逻辑
+ * 
  * @author 01140003
- * @version 2017年3月1日 下午5:18:04 
+ * @version 2017年3月1日 下午5:18:04
  */
 public class DropRuleHandler {
 	private static final Logger LOGGER = Logger.getLogger(DropRuleHandler.class);
+
 	public static void handle(ManagerConnection c, MycatDropRuleStatement stmt, String sql) {
 		MycatConfig mycatConfig = MycatServer.getInstance().getConfig();
-		Map<String, TableRuleConfig> tableRules = mycatConfig.getTableRules();
-		Map<String, AbstractPartitionAlgorithm> functions = mycatConfig.getFunctions();
-		
-		String ruleName = stmt.getRule();
-		
-		if (tableRules.get(ruleName) == null) {
-			c.writeErrMessage(ErrorCode.ER_RULE_NOT_EXIST, "Rule named " + ruleName + " dosen't exist.");
-			return;
-		} else {
+		mycatConfig.getLock().lock();
+
+		try {
+			Map<String, TableRuleConfig> tableRules = mycatConfig.getTableRules();
+			Map<String, AbstractPartitionAlgorithm> functions = mycatConfig.getFunctions();
+
+			String ruleName = stmt.getRule();
+			Map<String, SchemaConfig> schemas = mycatConfig.getSchemas();
+
+			if (tableRules.get(ruleName) == null) {
+				c.writeErrMessage(ErrorCode.ER_RULE_NOT_EXIST, "Rule named " + ruleName + " dosen't exist.");
+				return;
+			}
+
+			// 如果rule被schema使用，则不能被删除
+			for (SchemaConfig schemaConfig : schemas.values()) {
+				for (TableConfig tableConfig : schemaConfig.getTables().values()) {
+					RuleConfig ruleConfig = tableConfig.getRule();
+					if (ruleConfig == null) {
+						continue;
+					}else if (ruleConfig.getName().equals(ruleName)) {
+						c.writeErrMessage(ErrorCode.ER_CANT_DROP_RULE, "Rule named " + ruleName + " is in use.");
+						return;
+					}
+				}
+			}
 			// 拷贝tableRules
 			Map<String, TableRuleConfig> tempTableRules = new HashMap<String, TableRuleConfig>(tableRules);
 			tempTableRules.remove(ruleName);
-			
+
 			// 将修改刷到rule.xml中
-			try {
-				RuleJAXB ruleJAXB = JAXBUtil.toRuleJAXB(tempTableRules, functions);
-				if(!JAXBUtil.flushRule(ruleJAXB)) {
-					c.writeErrMessage(ErrorCode.ER_FLUSH_FAILED, "flush rule.xml fail");
-					return ;
-				}
-			} catch (Exception e) {
+			RuleJAXB ruleJAXB = JAXBUtil.toRuleJAXB(tempTableRules, functions);
+			if (!JAXBUtil.flushRule(ruleJAXB)) {
 				c.writeErrMessage(ErrorCode.ER_FLUSH_FAILED, "flush rule.xml fail");
-				LOGGER.error("flush rule.xml fail",e);
-				return ;
+				return;
 			}
-			
+
 			tableRules.remove(ruleName);
-			
-			// 向客户端端发送ok包
-			ByteBuffer buffer = c.allocate();
-			c.write(c.writeToBuffer(OkPacket.OK, buffer));
+		} catch (Exception e) {
+			c.writeErrMessage(ErrorCode.ER_FLUSH_FAILED, "flush rule.xml fail");
+			LOGGER.error("flush rule.xml fail", e);
+			return;
+		} finally {
+			mycatConfig.getLock().unlock();
 		}
+
+		// 向客户端端发送ok包
+		ByteBuffer buffer = c.allocate();
+		c.write(c.writeToBuffer(OkPacket.OK, buffer));
 	}
 }

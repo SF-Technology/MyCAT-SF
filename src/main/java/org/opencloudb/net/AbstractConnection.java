d04e88a65da0e960316bf7c4a78f477043434c2e
顺丰科技开源 <opensource@sfmail.sf-express.com>
2018-01-05 10:09:01 +0800

    修改包乱序的问题

--- src/main/java/org/opencloudb/net/AbstractConnection.java
+++ src/main/java/org/opencloudb/net/AbstractConnection.java
@@ -444,13 +444,14 @@ public abstract class AbstractConnection implements NIOConnection {
 	public void close(String reason) {
 		if (!isClosed.get()) {
 			closeSocket();
-			isClosed.set(true);
 			if (processor != null) {
 				processor.removeConnection(this);
 			}
 			this.cleanup();
 			isSupportCompress=false;
-
+			
+			//修改Got packet 1156包乱序的错误
+			isClosed.set(true);
 			//ignore null information
 			if (Strings.isNullOrEmpty(reason)) {
 				return;
